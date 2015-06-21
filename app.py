989d6f21510fcb297e5e6ecb806f0f55aa63bec4
Daniel kobina <danielkobina@gmail.com>
2015-06-21 18:09:49 -0400

    removed app test content

--- app.py
+++ app.py
@@ -1,15 +1,3 @@
 #!/usr/bin/env python
 
-annee = input("Saisissez une annee : ") 
-
-annee = int(annee) 
-
-if annee % 400 == 0 or (annee % 4 == 0 and annee % 100 != 0):
-
-    print("L'annee saisie est bissextile.")
-
-else:
-
-    print("L'annee saisie n'est pas bissextile.")
-
-raw_input()
+#The main application 
